7237fc6351bb4352127d4d105588817bbcc78815
Patrick Fraune <p.fraune@gmail.com>
2018-04-16 21:00:57 +0200

    init

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, Component} from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material/material.module';
+import { FormsModule } from '@angular/forms';
+import { MatTableModule, MatPaginatorModule} from '@angular/material';
+import { CdkTableModule } from '@angular/cdk/table';
+
+import { AppComponent } from './app.component';
+import { TableTestComponent } from './table-test/table-test.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TableTestComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    FormsModule,
+    MatTableModule, 
+    MatPaginatorModule, 
+    CdkTableModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
